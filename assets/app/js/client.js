871122464ed8843a7835ec60b0fd9e16db1f3eea
Zhao J <openbook114@sohu.com>
2016-08-29 09:23:11 +0800

    test002

--- assets/app/js/client.js
+++ assets/app/js/client.js
@@ -16,7 +16,7 @@ var userid=getQueryString("userid");
 
 //连接websocket后端服务器
 //var socket = io.connect('ws://haishen-comments.daoapp.io/haishen');
-var socket = io.connect('ws://fingermail-comments.daoapp.io/haishen');
+var socket = io.connect('ws://newdirect-comments.daoapp.io/haishen');
 //var socket= io.connect('ws://localhost:3000/haishen');
 //var socket= io.connect('ws://4k.evideocloud.com/haishen');
 
