2965c24b09b26bd9136d2e609a59b0ba6e1d79e6
Zhao J <openbook114@sohu.com>
2016-08-29 12:09:22 +0800

    test002

--- assets/app/js/stat.js
+++ assets/app/js/stat.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?dcc2b3ed2f83f6de80c43e333e776a93";
+  hm.src = "//hm.baidu.com/hm.js?ba6a547b5f7ac7c62db69beaa3a2af69";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
